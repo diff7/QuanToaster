40233e81640310d7113bba7dbab9c61aa4d7336f
som@bo.dy <jello@ex.con>
2021-10-02 02:18:03 +0300

    drop sr

--- sr_models/search_cnn.py
+++ sr_models/search_cnn.py
@@ -38,7 +38,7 @@ class SearchCNN(nn.Module):
 
         # for block in self.net:
         #     x = block(x, weight_alphas)
-        return self.pixelup(self.cnn_out(state_zero))
+        return self.pixelup(self.cnn_out(state_zero))+first_state
 
     def fetch_weighted_flops_and_memory(self, weight_alpha):
         flop = 0
