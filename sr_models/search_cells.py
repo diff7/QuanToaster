e3a31b5f5bbfd04bf3d04e45e57691c8d56eb0ce
JOKER <you@example.com>
2021-10-21 17:46:39 +0300

    debug

--- sr_models/search_cells.py
+++ sr_models/search_cells.py
@@ -137,5 +137,6 @@ class SearchArch(nn.Module):
             flops, memory = summer(
                 (flops, memory), cell.fetch_info(alphas["body"], alphas["skip"])
             )
-
+        print(flops)
+        print(alphas)
         return flops, memory
