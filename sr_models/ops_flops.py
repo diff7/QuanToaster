3c92125f97bbf48e4d53063f6990900613fbe100
JOKER <you@example.com>
2021-10-21 18:13:28 +0300

    debug

--- sr_models/ops_flops.py
+++ sr_models/ops_flops.py
@@ -445,8 +445,7 @@ class MixedOp(nn.Module):
         memory = 0
 
         for w, op in zip(weights, self._ops):
-            print("W:", w.shape)
-            print("W:", len(self._ops))
+            print("W:", w.shape, len(self._ops))
             flops, memory = self.summer(
                 (flops, memory), (w * v for v in op.fetch_info())
             )
