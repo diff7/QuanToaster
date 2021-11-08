e1a623e95cffc8e6547fc118ffab86d98de3a60b
som@bo.dy <jello@ex.con>
2021-11-08 18:04:54 +0300

    clip lsq

--- sr_models/quant_conv_lsq.py
+++ sr_models/quant_conv_lsq.py
@@ -106,11 +106,11 @@ class LsqQuan(nn.Module):
         else:
             s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
         s_scale = grad_scale(self.s, s_grad_scale)
-
-        x = x / s_scale
+        print("SCALE:", s_scale)
+        x = x / (s_scale + 1e-5)
         x = torch.clamp(x, self.thd_neg, self.thd_pos)
         x = round_pass(x)
-        x = x * (s_scale + 1e-5)
+        x = x * s_scale
         return x
 
 
